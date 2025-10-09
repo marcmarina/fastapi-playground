4e03699663b078224b792cc72fb6a7b599313388
Marc Marina <mmarina@findmypast.com>
2025-10-09 16:02:07 +0200

    Added metrics

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+instrumentator = Instrumentator(excluded_handlers=["/_health", "/metrics"])
+
+instrumentator.instrument(app).expose(app)
+
+
+@app.get("/")
+def root():
+    return "Welcome to FastAPI Playground"
 
 
 @app.get("/_health")
